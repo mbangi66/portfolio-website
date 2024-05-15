029e3846553768337aea0d906cca5876dd834aec
Mustaqeem <mbangi66@gmail.com>
2024-05-15 13:15:16 +0300

    updated thumbnails

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -19,8 +19,8 @@ import { Component } from '@angular/core';
     				<div class="project img ftco-animate d-flex justify-content-center align-items-center" style="background-image: url(../assets/images/work-1.jpg);">
     					<div class="overlay"></div>
 	    				<div class="text text-center p-4">
-	    					<h3><a href="#">Branding &amp; Illustration Design</a></h3>
-	    					<span>Web Design</span>
+	    					<h3><a href="https://hijabo.in/">Hijabo.in</a></h3>
+	    					<span>Hijabo provides curated collections that cater to the modern Muslim woman's style needs. With a focus on quality, affordability, and inclusivity, Hijabo aims to empower women to express their individuality while maintaining modesty</span>
 	    				</div>
     				</div>
   				</div>
@@ -28,8 +28,8 @@ import { Component } from '@angular/core';
     				<div class="project img ftco-animate d-flex justify-content-center align-items-center" style="background-image: url(../assets/images/work-2.jpg);">
     					<div class="overlay"></div>
 	    				<div class="text text-center p-4">
-	    					<h3><a href="#">Branding &amp; Illustration Design</a></h3>
-	    					<span>Web Design</span>
+	    					<h3><a href="https://serpelevator.com/">serpelevator.com</a></h3>
+	    					<span>Serpelevator is your go-to SAAS SEO agency, specializing in boosting organic traffic to your website. Let us optimize your online presence and drive targeted audiences to your business.</span>
 	    				</div>
     				</div>
   				</div>
@@ -38,8 +38,8 @@ import { Component } from '@angular/core';
     				<div class="project img ftco-animate d-flex justify-content-center align-items-center" style="background-image: url(../assets/images/work-3.jpg);">
     					<div class="overlay"></div>
 	    				<div class="text text-center p-4">
-	    					<h3><a href="#">Branding &amp; Illustration Design</a></h3>
-	    					<span>Web Design</span>
+	    					<h3><a href="https://eemanagement2024-4mcsi6h23q-uc.a.run.app/">Employees Management System | Dashboard</a></h3>
+	    					<span>Employees Management System Dashboard: A comprehensive solution for efficient employee management, streamlining tasks, tracking performance, and optimizing workflows</span>
 	    				</div>
     				</div>
     			</div>
