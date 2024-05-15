b83f73b9876564afab7c780a8bc08dd58684461d
Mustaqeem <mbangi66@gmail.com>
2024-05-15 17:06:53 +0300

    img changed and added style for them

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -46,23 +46,23 @@ import { Component } from '@angular/core';
     </section>
     <section class="ftco-section ftco-partner">
     	<div class="container">
-    		<div class="row">
-    			<div class="col-sm ftco-animate">
-    				<a href="#" class="partner"><img src="../assets/images/partner-1.png" class="img-fluid" alt="Colorlib Template"></a>
-    			</div>
-    			<div class="col-sm ftco-animate">
-    				<a href="#" class="partner"><img src="../assets/images/partner-2.png" class="img-fluid" alt="Colorlib Template"></a>
-    			</div>
-    			<div class="col-sm ftco-animate">
-    				<a href="#" class="partner"><img src="../assets/images/partner-3.png" class="img-fluid" alt="Colorlib Template"></a>
-    			</div>
-    			<div class="col-sm ftco-animate">
-    				<a href="#" class="partner"><img src="../assets/images/partner-4.png" class="img-fluid" alt="Colorlib Template"></a>
-    			</div>
-    			<div class="col-sm ftco-animate">
-    				<a href="#" class="partner"><img src="../assets/images/partner-5.png" class="img-fluid" alt="Colorlib Template"></a>
-    			</div>
-    		</div>
+        <div class="row justify-content-center">
+            <div class="col-6 col-md-3 col-lg-2 ftco-animate mb-4">
+                <img src="../assets/images/partner-1.png" class="img-fluid partner" alt="C#"> 
+            </div>
+            <div class="col-6 col-md-3 col-lg-2 ftco-animate mb-4">
+                <img src="../assets/images/partner-2.png" class="img-fluid partner" alt="DotNET"> 
+            </div>
+            <div class="col-6 col-md-3 col-lg-2 ftco-animate mb-4">
+                <img src="../assets/images/partner-3.png" class="img-fluid partner" alt="Angular"> 
+            </div>
+            <div class="col-6 col-md-3 col-lg-2 ftco-animate mb-4">
+                <img src="../assets/images/partner-4.png" class="img-fluid partner" alt="SQL"> 
+            </div>
+            <div class="col-6 col-md-3 col-lg-2 ftco-animate mb-4">
+                <img src="../assets/images/partner-5.png" class="img-fluid partner" alt="Node.js"> 
+            </div>
+        </div>
     	</div>
     </section>
   `,
